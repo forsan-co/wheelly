feadb637ea3f1d548d2ffc6d077802b1a82ba6f3
Zhuk Sergey <seregazhuk88@gmail.com>
2016-07-30 20:56:09 +0300

    leap year page added

--- src/app.php
+++ src/app.php
@@ -2,6 +2,9 @@
 
 use Symfony\Component\Routing\Route;
 use Symfony\Component\Routing\RouteCollection;
+use Symfony\Component\HttpFoundation\Response;
+
+require_once __DIR__ . '/../src/helpers.php';
 
 $routes = new RouteCollection();
 $routes->add('hello', new Route('/hello/{name}', array(
@@ -10,6 +13,15 @@ $routes->add('hello', new Route('/hello/{name}', array(
     ))
 );
 $routes->add('bye', new Route('/bye', array('_controller' => 'render_template')));
+$routes->add('leap_year', new Route('/is_leap_year/{year}', array(
+    'year' => null,
+    '_controller' => function($request) {
+        if(is_leap_year($request->attributes->get('year'))) {
+            return new Response('Yes, this is a leap year!');
+        }
 
+        return new Response('No, this is not a leap year!');
+    }
+)));
 
 return $routes;
