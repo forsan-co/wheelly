f2c7a72039dbe65caa387eba79a1f0da355132b8
Zhuk Sergey <seregazhuk88@gmail.com>
2016-07-30 20:49:10 +0300

    render template controller

--- src/app.php
+++ src/app.php
@@ -4,7 +4,12 @@ use Symfony\Component\Routing\Route;
 use Symfony\Component\Routing\RouteCollection;
 
 $routes = new RouteCollection();
-$routes->add('hello', new Route('/hello/{name}', array('name' => 'World')));
-$routes->add('bye', new Route('/bye'));
+$routes->add('hello', new Route('/hello/{name}', array(
+        'name' => 'World',
+        '_controller' => 'render_template',
+    ))
+);
+$routes->add('bye', new Route('/bye', array('_controller' => 'render_template')));
+
 
 return $routes;
