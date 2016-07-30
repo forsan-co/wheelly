c15caa86c9c453e46a968cf4c93b9718b6718da6
Zhuk Sergey <seregazhuk88@gmail.com>
2016-07-30 19:14:51 +0300

    test added

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 
 $input = isset($_GET['name']) ? $_GET['name'] : 'World';
 
-header('Content-Type: text/html; charset=utf-8');
+//header('Content-Type: text/html; charset=utf-8');
 
 printf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8'));
