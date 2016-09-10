2dfa68e6f91573e97474ef363705d484995bcfc1
Zhuk Sergey <seregazhuk88@gmail.com>
2016-09-10 21:13:45 +0300

    query builder

---
+++ database/Connection.php
@@ -0,0 +1,18 @@
+<?php
+
+class Connection 
+{
+    public static function make()
+    {
+        try {
+            return new PDO('mysql:host=127.0.0.1;dbname=todo', 'root', 'root');
+        }
+        catch(PDOException $e) {
+            die($e->getMessage());
+        }
+    }
+}
+
+
+
+
--- functions.php
+++ functions.php
@@ -1,22 +1,5 @@
 <?php
 
-function connectToDb(){
-    try {
-        return new PDO('mysql:host=127.0.0.1;dbname=todo', 'root', 'root');
-    }
-    catch(PDOException $e) {
-        die('Could not connect');
-    }
-}
-
-function fetchAllTasks($pdo) {
-    $statement = $pdo->prepare('SELECT * FROM todos');
-
-    $statement->execute();
-
-    return $statement->fetchAll(PDO::FETCH_CLASS, 'Task');
-}
-
 function dd($data) {
     echo '<pre>';
     die(var_dump($data));
