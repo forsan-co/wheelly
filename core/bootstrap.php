7fa0839a6d343bbc9ebc9283fb9daf1eb2de8d91
Zhuk Sergey <seregazhuk88@gmail.com>
2016-10-01 11:53:49 +0300

    composer autoload added

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -1,11 +1,5 @@
 <?php
 
-require 'core/Application.php';
-require 'core/Router.php';
-require 'core/database/Connection.php';
-require 'core/database/QueryBuilder.php';
-require 'core/Request.php';
-
 $app = new Application();
 
 $app['config'] = require 'config.php';
